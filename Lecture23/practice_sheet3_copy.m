ca1db02fc49ae106257df2c5a1c8727fe29ddf07
Bhagyaaa19 <108164547+Bhagyaaa19@users.noreply.github.com>
2024-04-02 01:41:23 +0530

    Add files via upload

---
+++ Lecture23/practice_sheet3_copy.m
@@ -0,0 +1,4 @@
+clear all; close all; clc;
+a = 35
+b = [ 1 2 3 4]
+b
---
+++ Lecture23/sheet2.m
@@ -0,0 +1,8 @@
+
+# exponential terms are solved first by Octave
+# solves * and / at the same time
+# then only it solves + and -
+# if brackets are not given, this is the order
+# try to use brackets and see as well
+
+
